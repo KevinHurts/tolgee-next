d12974f1210dc215e16efb6bb974e1eda7d5290c
Kevin Hurts <kevinhurts@gmail.com>
2025-03-27 21:12:38 +0100

    Add cookies

--- src/utils/i18nRequestConfig.ts
+++ src/utils/i18nRequestConfig.ts
@@ -1,6 +1,6 @@
 import { getRequestConfig } from "next-intl/server";
 import { staticData } from "./tolgee";
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 
 export default getRequestConfig(async ({ requestLocale }) => {
     const locale = await requestLocale;
@@ -12,14 +12,14 @@ export default getRequestConfig(async ({ requestLocale }) => {
     }
 
     // Get locale from session
-    // const cookieStore = await cookies();
-    // const rtyloc = cookieStore.get("rtyloc");
-    // console.log({ rtyloc });
-    // if (rtyloc?.value) {
-    //     language = rtyloc.value.split(".")[0].split("-")[0];
-    // } else {
-    //     language = "en";
-    // }
+    const cookieStore = await cookies();
+    const rtyloc = cookieStore.get("rtyloc");
+    console.log({ rtyloc });
+    if (rtyloc?.value) {
+        language = rtyloc.value.split(".")[0].split("-")[0];
+    } else {
+        language = "en";
+    }
 
     return {
         // do this to make next-intl not emit any warnings
